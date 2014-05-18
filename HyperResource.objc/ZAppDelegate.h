8b34d1285c3e44b7c5ca51799bf61330a9c9a794
Ayal Spitz <bitbucket@blackgecko.com>
2014-05-18 08:15:19 -0400

    Initial Commit

---
+++ HyperResource.objc/ZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZAppDelegate.h
+//  HyperResource.objc
+//
+//  Created by Ayal Spitz on 5/18/14.
+//  Copyright (c) 2014 Ayal Spitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HyperResource.objc/ZViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZViewController.h
+//  HyperResource.objc
+//
+//  Created by Ayal Spitz on 5/18/14.
+//  Copyright (c) 2014 Ayal Spitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZViewController : UIViewController
+
+@end
